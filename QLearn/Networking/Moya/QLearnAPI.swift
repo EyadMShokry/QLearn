8c9364e28618491b3fd2fd5ebe4d205f9f6cf8f9
Eyad Shokry <eyadmshokry@gmail.com>
2020-11-21 21:31:53 +0200

    Finishing Live Exam APIs using Moya

--- QLearn/Networking/Moya/QLearnAPI.swift
+++ QLearn/Networking/Moya/QLearnAPI.swift
@@ -11,11 +11,13 @@ import Moya
 public enum QLearnAPI {
     // MARK: Live Exam APIs
     // LE -> Live Exam
-    case insertEssayQuestionLE(essay_type: String, exam_id: String, teacher_id: String, level: String)
-    case insertMcqQuestionLE(correct_answer: String, exam_id: String, teacher_id: String, level: String)
-    case insertTFQuestionLE(explanation: String, exam_id: String, teacher_id: String, level: String)
+    case insertEssayQuestionLE(question: String, correct_answer: String, essay_type: String, exam_id: String, teacher_id: String, level: String)
+    case insertMcqQuestionLE(question: String, choice1: String, choice2: String,
+        choice3 : String, choice4: String, correct_answer: String, exam_id: String, teacher_id: String, level: String)
+    case insertTFQuestionLE(question: String, correct_mark: String, explanation: String, exam_id: String, teacher_id: String, level: String)
     case updateEssayQuestionLE(id: String, question: String, correct_answer: String)
-    case updateMcqQuestionLE(id: String, question: String, choice1: String, choice2: String)
+    case updateMcqQuestionLE(id: String, question: String, choice1: String, choice2: String,
+        choise3: String, choise4: String, correct_answer: String, level: String)
     case updateTFQuestionLE(id: String, question: String, correct_answer: String, explanation: String)
     case deleteLEQuestion(id: String, q_type: String)
     case selectExam(id: String)
@@ -74,14 +76,57 @@ extension QLearnAPI: TargetType {
     
     public var task: Task {
         switch self {
-        case .contactUs(userName: let userName, jobPosition: let jobPosition, phoneNumber: let phoneNumber, message: let message):
-            return .requestParameters(parameters: ["user_name": userName, "job_position": jobPosition, "phone_number": phoneNumber, "message": message], encoding: URLEncoding.queryString)
-//        case .getAdsByCountry(countryCode: let countryCode, start: let start, limit: let limit): return .requestParameters(parameters: ["type": "ads", "country": countryCode, "limit": limit, "start": start], encoding: URLEncoding.queryString)
-//        case .getLocation(ipAddress: let ipAddress):
-//            return .requestParameters(parameters: ["ip": ipAddress], encoding: URLEncoding.queryString)
-//        case .getCategories: return .requestParameters(parameters: ["type": "catogaries"], encoding: URLEncoding.queryString)
-//        case .getCountries: return .requestParameters(parameters: ["type": "countries"], encoding: URLEncoding.queryString)
-//        case .getAds(countryCode: let countryCode, regionCode: let regionCode, category: let category): return .requestParameters(parameters: ["type": "ads", "country": countryCode, "region": regionCode ,"cat": category], encoding: URLEncoding.queryString)
+        // MARK: Live Exam APIs
+        case .insertEssayQuestionLE(question: let question, correct_answer: let correctAnswer, essay_type: let essayType, exam_id: let examId, teacher_id: let teacherId, level: let level) :
+            return .requestParameters(parameters: ["question" : question, "correct_answer" : correctAnswer, "essay_type" : essayType, "exam_id" : examId, "teacher_id" : teacherId, "level" : level], encoding: URLEncoding.queryString)
+            
+        case .insertMcqQuestionLE(question: let question, choice1: let choice1, choice2: let choice2, choice3: let choice3, choice4: let choice4, correct_answer: let correctAnswer, exam_id: let examId, teacher_id: let teacherId, level: let level) :
+            return .requestParameters(parameters: ["question" : question, "choice1" : choice1, "choice2" : choice2, "choice3" : choice3, "choice4" : choice4,"correct_answer" : correctAnswer, "exam_id" : examId, "teacher_id" : teacherId, "level" : level], encoding: URLEncoding.queryString)
+            
+        case .insertTFQuestionLE(question: let question, correct_mark: let correctMark, explanation: let explanation, exam_id: let examId, teacher_id: let teacherId, level: let level) :
+            return .requestParameters(parameters: ["question" : question, "correct_mak" : correctMark,"explanation" : explanation, "exam_id" : examId, "teacher_id" : teacherId, "level" : level], encoding: URLEncoding.queryString)
+            
+        case .updateEssayQuestionLE(id: let id, question: let question, correct_answer: let correctAnswer) :
+            return .requestParameters(parameters: ["id" : id, "question" : question, "correct_answer" : correctAnswer], encoding: URLEncoding.queryString)
+            
+        case . updateMcqQuestionLE(id: let id, question: let question, choice1: let choice1, choice2: let choice2, choise3: let choice3, choise4: let choice4, correct_answer: let correctAnswer, level: let level) :
+            return .requestParameters(parameters: ["id" : id, "question" : question, "choice1" : choice1, "choice2" : choice2, "choice3" : choice3, "choice4" : choice4 ,"correct_answer" : correctAnswer, "level" : level], encoding: URLEncoding.queryString)
+            
+        case .updateTFQuestionLE(id: let id, question: let question, correct_answer: let correctAnswer, explanation: let explanation) :
+            return .requestParameters(parameters: ["id" : id, "question" : question, "correct_answer" : correctAnswer, "explanation" : explanation], encoding: URLEncoding.queryString)
+            
+        case .deleteLEQuestion(id: let id, q_type: let questionType) :
+            return .requestParameters(parameters: ["id" : id, "q_type" : questionType], encoding: URLEncoding.queryString)
+            
+        case .selectExam(id: let id) :
+            return .requestParameters(parameters: ["id" : id], encoding: URLEncoding.queryString)
+            
+        case .selectAllLEQuestions(level: let level, teacher_id: let teacherId, q_type: let qType) :
+            return .requestParameters(parameters: ["level" : level, "teacher_id" : teacherId, "q_type" : qType], encoding: URLEncoding.queryString)
+            
+        case .selectAnswerdEssayQuestionsLE(exam_id: let examId, teacher_id: let teacherId, student_id: let studentId, level: let level) :
+            return .requestParameters(parameters: ["exam_id" : examId, "teacher_id" : teacherId, "student_id" : studentId, "level" : level], encoding: URLEncoding.queryString)
+            
+        case .selectAnswerMcqQuestionsLE(exam_id: let examId, teacher_id: let teacherId, student_id: let studentId, level: let level) :
+            return .requestParameters(parameters: ["exam_id" : examId, "teacher_id" : teacherId, "student_id" : studentId, "level" : level], encoding: URLEncoding.queryString)
+            
+        case .selectAnswerdTFQuestionsLE(exam_id: let examId, teacher_id: let teacherId, student_id: let studentId, level: let level) :
+            return .requestParameters(parameters: ["exam_id" : examId, "teacher_id" : teacherId, "student_id" : studentId, "level" : level], encoding: URLEncoding.queryString)
+            
+        case .selectCountOfQuestionsLE(teacher_id: let teacherId, level: let level) :
+            return .requestParameters(parameters: ["teacher_id" : teacherId, "level" : level], encoding: URLEncoding.queryString)
+            
+        case .selectNotAnswerdEssayQuestionsLE(exam_id: let examId, teacher_id: let teacherId, student_id: let studentId, level: let level) :
+            return .requestParameters(parameters: ["exam_id" : examId, "teacher_id" : teacherId, "student_id" : studentId, "level" : level], encoding: URLEncoding.queryString)
+            
+        case .selectNotAnswerdMcqQuestionsLE(exam_id: let examId, teacher_id: let teacherId, student_id: let studentId, level: let level) :
+            return .requestParameters(parameters: ["exam_id" : examId, "teacher_id" : teacherId, "student_id" : studentId, "level" : level], encoding: URLEncoding.queryString)
+            
+        case .selectNotAnswerdTFQuestionsLE(exam_id: let examId, teacher_id: let teacherId, student_id: let studentId, level: let level) :
+            return .requestParameters(parameters: ["exam_id" : examId, "teacher_id" : teacherId, "student_id" : studentId, "level" : level], encoding: URLEncoding.queryString)
+            
+        default :
+            return .requestPlain
         }
     }
     
