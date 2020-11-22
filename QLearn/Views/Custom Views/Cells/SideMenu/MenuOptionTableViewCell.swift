50aea0bc8f94a0b6ab9f7bdb39096e2f38570765
Eyad Shokry <eyadmshokry@gmail.com>
2020-11-22 23:02:27 +0200

    Student ID QR Code is done

---
+++ QLearn/Views/Custom Views/Cells/SideMenu/MenuOptionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuOptionTableViewCell.swift
+//  QLearn
+//
+//  Created by Eyad Shokry on 11/22/20.
+//  Copyright © 2020 Eyad Shokry. All rights reserved.
+//
+
+import UIKit
+
+class MenuOptionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
